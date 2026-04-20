bacb0d3adc229b751c2c0b3598f3fba3b1906035
Anvarsha <anvarshaanvar576@gmail.com>
2026-04-20 09:49:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.js
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.js
@@ -2,24 +2,47 @@
  * @param {number[]} colors
  * @return {number}
  */
-var maxDistance = function(colors) {
- let i = 0
- let j = colors.length-1
- let k = colors.length-1
- let l = 0
- let maxDis = 0
- while(i<j){
-      if(colors[i]===colors[j]){
-        j--
-      }
-      if(colors[l]===colors[k]){
-        l++
-      }
-      else{
-        maxDis = Math.max(j-i,k-l,maxDis)
-        j--
-        l++
-      }
- }  
- return maxDis
+var maxDistance = function (colors) {
+    // let i = 0
+    // let j = colors.length - 1
+
+    // let k = colors.length - 1
+    // let l = 0
+
+    // let maxDis = 0
+    // while (i < j) {
+    //     if (colors[i] === colors[j]) {
+    //         j--
+    //     }
+    //     if (colors[l] === colors[k]) {
+    //         l++
+    //     }
+    //     else {
+    //         maxDis = Math.max(j - i, k - l, maxDis)
+    //         j--
+    //         l++
+    //     }
+    // }
+    // return maxDis
+   
+   let first = 0;
+   let second = 0;
+   let j = colors.length-1;
+   let k = 0;
+
+    for(let i=0;i<colors.length;i++){
+        if(colors[i]!==colors[j]){
+            first = j-i
+            break
+        }
+    }
+
+     for(let i=colors.length-1;i>0;i--){
+        if(colors[i]!==colors[k]){
+            second = i-k
+            break
+        }
+    }
+      console.log(first,second)
+    return Math.max(first,second)
 };
