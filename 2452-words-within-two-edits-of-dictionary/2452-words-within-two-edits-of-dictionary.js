6e10d05fa6751fa088aa89e74390891c7a165f59
Anvarsha <anvarshaanvar576@gmail.com>
2026-04-22 10:58:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.js
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.js
@@ -8,38 +8,35 @@ var twoEditWords = function (queries, dictionary) {
 
     for (let i = 0; i < queries.length; i++) {
         for (let j = 0; j < dictionary.length; j++) {
-            // console.log(queries[i],dictionary[j])
-            if (queries[i] === dictionary[j] && result[result.length-1]!==queries) {
+            if (queries[i] === dictionary[j] && result[result.length - 1] !== queries) {
                 result.push(queries[i])
                 break
             }
             else {
-           
-              
-             let added =  checkMatching(queries[i],dictionary[j],result)
-            if(added==true)break
+                let added = checkMatching(queries[i], dictionary[j], result)
+                if (added == true) break
             }
         }
     }
-   return result
+    return result
 };
 
-function checkMatching(q,d,r){
-     let limit = 0
-     let k = 0
-     let added = false
-           while(k<q.length){
-              if(q[k]!==d[k]){
-                limit++
-              }    
-              k++
-           }  
-           
-        if(limit<3){
-        if(!r.includes(q)){
+function checkMatching(q, d, r) {
+    let limit = 0
+    let k = 0
+    let added = false
+    while (k < q.length) {
+        if (q[k] !== d[k]) {
+            limit++
+        }
+        k++
+    }
+
+    if (limit < 3) {
+        if (!r.includes(q)) {
             r.push(q)
             added = true
         }
         return added
-         }
+    }
 }
