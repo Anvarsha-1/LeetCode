5d369d6207c5e88a51251e1e166be56d62fd9cff
Anvarsha <anvarshaanvar576@gmail.com>
2026-04-24 12:49:20 +0530

    Time: 28 ms (6.88%), Space: 56.6 MB (11.11%) - LeetHub

---
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var uniqueMorseRepresentations = function(words) {
+    if(words.length==1) return 1
+    dicti = {'a':".-", 'b':"-...", 'c':"-.-.", 'd':"-..", 'e':".", 'f':"..-.", 'g':"--.", 'h':"....", 'i':"..", 'j':".---", 'k':"-.-", 'l':".-..", 'm':"--", 'n':"-.", 'o':"---", 'p':".--.", 'q':"--.-", 'r':".-.", 's':"...", 't':"-", 'u':"..-", 'v':"...-", 'w':".--", 'x':"-..-", 'y':"-.--", 'z':"--.."}
+    let seen =  new Set()
+    let count = 0
+for(let i=0;i<words.length;i++){
+    let mouseWord = ""
+    for(let j=0;j<words[i].length;j++){
+        // console.log(words[i][j])
+         mouseWord +=dicti[words[i][j]]
+    }
+    console.log(mouseWord)
+    if(seen.has(mouseWord)){
+        
+    }else{
+        seen.add(mouseWord)
+        count++
+    }
+}return count
+
+};
