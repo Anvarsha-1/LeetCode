c5a9c0e239d93213b5e0199655bd9301551bd6ee
Anvarsha <anvarshaanvar576@gmail.com>
2026-04-24 17:42:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2833-furthest-point-from-origin/2833-furthest-point-from-origin.js
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.js
@@ -7,13 +7,9 @@ var furthestDistanceFromOrigin = function(moves) {
     let right = 0
     let dash =  0
     for(let i=0;i<moves.length;i++){
-        if(moves[i]==="L"){
-            left++
-        }else if(moves[i]==="R"){
-            right++
-        }else{
-            dash++
-        }
+        if(moves[i]==="L")left++
+        if(moves[i]==="R")right++
+        if(moves[i]==="_") dash++
     }
     return Math.abs(left-right)+dash
 };
