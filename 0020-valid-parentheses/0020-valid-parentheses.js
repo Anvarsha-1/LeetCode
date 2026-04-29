fa862b41479baba947d98af1041f2dde6e657bd8
Anvarsha <anvarshaanvar576@gmail.com>
2026-04-29 10:24:17 +0530

    Time: 3 ms (70.24%), Space: 56.5 MB (35.73%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    if(s.length<=1) return false
+    let map = {"]":"[","}":"{",")":"("}
+    let stack = []
+   for(let i=0;i<s.length;i++){
+    if(s[i]==="(" || s[i]==="[" || s[i]==="{"){
+        stack.push(s[i])
+    }else{
+        if(map[s[i]]!==stack.pop()){
+            return false
+        }
+    }
+   } return stack.length===0
+};
