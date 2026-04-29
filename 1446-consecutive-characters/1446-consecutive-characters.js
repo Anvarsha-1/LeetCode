09c1f578e0e80fcfe27bd88bd8e56523948d7531
Anvarsha <anvarshaanvar576@gmail.com>
2026-04-29 13:49:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.js
+++ 1446-consecutive-characters/1446-consecutive-characters.js
@@ -5,9 +5,7 @@
 var maxPower = function(s) {
     let power = 0
     let maxUnique = 1
-
     for(let i=0;i<=s.length-1;i++){
-        console.log(s[i],s[i+1])
         if(s[i]===s[i+1]){
             maxUnique+=1
         }else{
