db1fd8f3d292a41ca5f3922022e0d112f8df35a5
Anvarsha <anvarshaanvar576@gmail.com>
2026-05-01 21:34:03 +0530

    Time: 0 ms (100%), Space: 55.3 MB (56.88%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+    if(n==1)return true
+    let number = n
+    let seen = new Set()
+    while(n){
+        let resulttrack = 0
+        let str = number.toString().split("")
+        for(let num of str){
+             resulttrack +=Number(num*num)
+        }
+        if(seen.has(resulttrack)){
+            return false
+        }
+        seen.add(resulttrack)
+        number = resulttrack
+        if(resulttrack===1) return true
+    }return false
+};
