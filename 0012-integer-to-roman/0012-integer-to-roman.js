27b4131e5c1e51bbfbaeee2ef116f41e3e330e22
Anvarsha <anvarshaanvar576@gmail.com>
2026-05-03 19:59:59 +0530

    Time: 48 ms (5.51%), Space: 62.9 MB (36.28%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -0,0 +1,56 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function (num) {
+    let symbol = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 }
+    let result = ""
+    while (num) {
+        if (num >= 1000) {
+            num -= 1000
+            result += "M"
+        }else if(num>=900){
+            num-=900
+            result+="CM"
+        }
+        else if (num >= 500) {
+            num -= 500
+            result += "D"
+        }else if(num>=400){
+            num-=400
+            result+="CD"
+        } else if (num >= 100) {
+            num -= 100
+            result += "C"
+        }
+        else if(num>=90){
+            num-=90
+            result+="XC"
+        } else if (num >= 50) {
+            num -= 50
+            result += "L"
+        } else if (num >= 40) {
+            num -= 40
+            result += "XL"
+        }
+        else if (num >= 10) {
+            console.log(num)
+            num -= 10
+            result += "X"
+        } else if (num == 9) {
+            num -= 9
+            result += "IX"
+        }
+        else if (num >= 5) {
+            num -= 5
+            result += "V"
+        } else if (num == 4) {
+            num -= 4
+            result += "IV"
+        } else if (num >= 1) {
+            num -= 1
+            result += "I"
+        }
+    } return result
+
+};
