df7e4e762db07f20c752bd18f399b4fcca3fffc5
Anvarsha <anvarshaanvar576@gmail.com>
2026-05-03 16:18:59 +0530

    Time: 564 ms (4.84%), Space: 64.4 MB (5.04%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var rotateString = function (s, goal) {
+    if (s.length !== goal.length) return false
+    if (s === goal) return true
+    let str = s.split("")
+    for (let i = 0; i < str.length-1; i++) {
+        let first = str[0]
+        for (let j = 0; j < str.length-1; j++) {
+            str[j] = str[j + 1]
+            console.log(str.join(""))
+        }
+        str[str.length - 1] = first
+        console.log("after rotate",str.join(""))
+        if (str.join("") === goal) return true
+    } return false
+};
