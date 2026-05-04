1442a94c8dd1b4412dfa0ec684cbbcce7933cdde
Anvarsha <anvarshaanvar576@gmail.com>
2026-05-04 18:24:16 +0530

    Time: 1 ms (85.81%), Space: 53.1 MB (98.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,15 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-    let map = new Map()
-    for (let i = 0; i < nums.length; i++) {
-        map.set(nums[i], i)
-    }
+    let  hash = {}
 
-    for (let i = 0; i < nums.length; i++) {
-        let secondNumber = target - nums[i]
-        if (map.has(secondNumber) && map.get(secondNumber) !== i) {
-            return [i, map.get(secondNumber)]
+    for(let i=0;i<nums.length;i++){
+        let secondNum = target - nums[i]
+        if(hash[secondNum]>-1){
+            return [i,hash[secondNum]]
+        }else{
+            hash[nums[i]] =  i
         }
-    } return []
+    }
+    
 };
