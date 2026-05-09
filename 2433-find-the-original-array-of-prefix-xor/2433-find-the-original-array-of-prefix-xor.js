c590a6b0eeb04448673e3ee8942162438cb5efcc
Anvarsha <anvarshaanvar576@gmail.com>
2026-05-09 09:59:48 +0530

    Time: 959 ms (7.27%), Space: 80.9 MB (29.09%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.js
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.js
@@ -3,8 +3,10 @@
  * @return {number[]}
  */
 var findArray = function(pref) {
-    let arr = [pref[0]]
-    for(let i=0;i<pref.length-1;i++){
-        arr.push(pref[i]^pref[i+1])
-    }return arr
+    let prev = pref[0]
+    for(let i=1;i<pref.length;i++){
+        pref[i] ^= prev
+        prev ^= pref[i]
+        console.log(prev)
+    }return pref
 };
