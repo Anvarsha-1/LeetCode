563083e4e3464dc721b6dec34f2b03e5b1d77d04
Anvarsha <anvarshaanvar576@gmail.com>
2026-05-11 16:52:55 +0530

    Time: 87 ms (57.14%), Space: 65.9 MB (28.57%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.js
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.js
@@ -2,27 +2,23 @@
  * @param {string} word
  * @return {number}
  */
-var minimumPushes = function(word) {
-    if(word.length<=8)return word.length
+var minimumPushes = function (word) {
+    if (word.length <= 8) return word.length
     let map = {}
-    for(let val of word){
-        map[val] = (map[val]||0)+1
+    for (let val of word) {
+        map[val] = (map[val] || 0) + 1
     }
-    let sorted = Object.entries(map).sort((a,b)=> b[1]-a[1])
+    let sorted = Object.entries(map).sort((a, b) => b[1] - a[1])
 
-   let pushes = 0
-   let inc = 8
-   let k = 1
-   for(let i=0;i<sorted.length;i++){
-     for(let j=1;j<sorted[i].length;j++){
-       
-        if(i===inc){
-            k+=1
-            inc+=8
+    let pushes = 0
+    let inc = 8
+    let k = 1
+    for (let i = 0; i < sorted.length; i++) {
+        if (i === inc) {
+            k += 1
+            inc += 8
         }
-        pushes+=sorted[i][j] * k
-
-     }
-   }
-    return pushes 
+        pushes += sorted[i][1] * k
+    }
+    return pushes
 };
