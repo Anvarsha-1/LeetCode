89afdc98c06f524dc1a218300e4226c5aa5de3dc
Anvarsha <anvarshaanvar576@gmail.com>
2026-05-13 10:35:24 +0530

    Time: 1 ms (84.62%), Space: 57.6 MB (50%) - LeetHub

--- 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.js
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.js
@@ -2,17 +2,16 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var minBitwiseArray = function(nums) {
-   let result = []
-   
-   for(let i=0;i<nums.length;i++){
-    let half = Math.floor(nums[i]/2)
-    let or = (half| half+1)
-    if(or>nums[i]) {
-        result.push(-1)
-        continue
-    }
-    let find =  nums[i] - or
-    result.push(half+find) 
-   }return result
+var minBitwiseArray = function (nums) {
+
+    for (let i = 0; i < nums.length; i++) {
+        let half = Math.floor(nums[i] / 2)
+        let or = (half | half + 1)
+        if (or > nums[i]) {
+            nums[i] = -1
+            continue
+        }
+        let find = nums[i] - or
+        nums[i] = (find + half)
+    } return nums
 };
