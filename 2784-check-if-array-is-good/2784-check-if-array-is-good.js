2fbe369d8c36629e5ed162d28c487205d753fecf
Anvarsha <anvarshaanvar576@gmail.com>
2026-05-14 10:30:30 +0530

    Time: 4 ms (8.33%), Space: 55.9 MB (75%) - LeetHub

---
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isGood = function(nums) {
+   let max = Math.max(...nums) 
+   let hash = {}
+   for(let val of nums){
+    hash[val] = (hash[val]||0)+1
+   }
+   if(max>Object.keys(hash).length)return false
+   for(let key in hash){
+    if(hash[key]>1 && key!=max){
+        console.log(hash[key],key,max)
+        return false
+    }
+   }
+   return nums.length===max+1
+};
