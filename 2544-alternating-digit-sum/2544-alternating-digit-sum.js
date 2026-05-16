1bcfca13b97aedc09d50a0e52efce7fbe50eb5bb
Anvarsha <anvarshaanvar576@gmail.com>
2026-05-16 20:09:21 +0530

    Time: 0 ms (100%), Space: 53.9 MB (22.4%) - LeetHub

--- 2544-alternating-digit-sum/2544-alternating-digit-sum.js
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.js
@@ -5,14 +5,13 @@
 var alternateDigitSum = function(n) {
     let sum = 0;
     let string = n.toString()
-    let i=1
-    while(i<=string.length){
-        if(i%2!==0){
-            sum+=Number(string[i-1])
+    for(let i=0;i<string.length;i++){
+        let digit = Number(string[i])
+        if(i%2==0){
+            sum+=digit
         }else{
-            sum+=Number(string[i-1]-(string[i-1]*2))
+            sum-=digit
         }
-        i++
     }
     return sum
 };
