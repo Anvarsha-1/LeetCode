0c5d90d534730921d2ef9b5458599fc9715931a6
Anvarsha <anvarshaanvar576@gmail.com>
2026-05-19 12:03:36 +0530

    Time: 21 ms (19.71%), Space: 74.3 MB (19.23%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.js
+++ 2540-minimum-common-value/2540-minimum-common-value.js
@@ -4,23 +4,30 @@
  * @return {number}
  */
 var getCommon = function (nums1, nums2) {
-    let result = -1
-    for (let i=nums2.length-1;i>=0;i--) {
-      let values = searchValue(nums2[i],nums1)
-      result =  values ? values : result
-    }return result
+    // let result = -1
+    // for (let i=nums2.length-1;i>=0;i--) {
+    //   let values = searchValue(nums2[i],nums1)
+    //   result =  values ? values : result
+    // }return result
+   // -------------------------------------------
+   let set = new Set(nums1)
+   for(let val of nums2){
+    if(set.has(val)){
+        return val
+    }
+   }return -1
 };
 
-function searchValue(val,nums1) {
-    let left = 0
-    let right = nums1.length - 1
-    while (left <= right) {
-        let middle = Math.floor((left + right)/2)
-        if (nums1[middle] === val) return val
-        if(nums1[middle]>val){
-            right = middle-1
-        }else{
-            left = middle+1
-        }
-    }
-}
+// function searchValue(val,nums1) {
+//     let left = 0
+//     let right = nums1.length - 1
+//     while (left <= right) {
+//         let middle = Math.floor((left + right)/2)
+//         if (nums1[middle] === val) return val
+//         if(nums1[middle]>val){
+//             right = middle-1
+//         }else{
+//             left = middle+1
+//         }
+//     }
+// }
