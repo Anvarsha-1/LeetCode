f38ca21a6e3bf71dc9c1b11a63ae6b0882111a43
Anvarsha <anvarshaanvar576@gmail.com>
2026-05-20 17:35:04 +0530

    Time: 19 ms (97.27%), Space: 65 MB (98.95%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,27 +3,15 @@
  * @return {string[][]}
  */
 var groupAnagrams = function (strs) {
-    let result = []
-    let answer = []
-    let seen = {}
-    for (let i = 0; i < strs.length; i++) {
-        result[i] = strs[i].split("").sort().join("")
-    }
-    let index = 0
-    for (let i = 0; i < result.length; i++) {
-        let str = result[i]
-        if (seen[str]===undefined) {
-            seen[str] = index
-            index+=1
-        }
-         else {
-            answer[seen[str]].push(strs[i])
-        }
-        if (!answer[seen[str]]) {
-            answer[seen[str]] = []
-            answer[seen[str]].push(strs[i])
-        }
+    let result = new Map()
 
-    } 
-    return answer
+    for (let i = 0; i < strs.length; i++){
+        let char = strs[i].split("").sort().join("")
+        if(!result.has(char)){
+            result.set(char,[strs[i]])
+        }else{
+            result.get(char).push(strs[i])
+        }
+    }return Array.from(result.values())
+   
 };
