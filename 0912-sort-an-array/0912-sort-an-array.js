53d3bb67a0ad932703596f14d616253f89d10500
Anvarsha <anvarshaanvar576@gmail.com>
2026-05-24 21:51:01 +0530

    Time: 203 ms (14.53%), Space: 80.1 MB (59.39%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(nums) {
+   return MergeSort(nums)
+};
+
+
+function MergeSort(arr){
+    if(arr.length<=1)return arr
+
+    let middle = Math.floor(arr.length/2)
+
+    let left = MergeSort(arr.slice(0,middle))
+    let right = MergeSort(arr.slice(middle))
+
+    return Merge(left,right)
+}
+
+function Merge(left,right){
+    let leftIndex = 0
+    let rightIndex = 0
+    let result = []
+
+    while(leftIndex<left.length && rightIndex<right.length){
+        if(left[leftIndex]<right[rightIndex]){
+            result.push(left[leftIndex])
+            leftIndex++
+        }else{
+            result.push(right[rightIndex])
+            rightIndex++
+        }
+    }
+
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex))  
+}
