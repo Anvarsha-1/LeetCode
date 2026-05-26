82be802141d5d45be0646c5ae423be138e228a99
Anvarsha <anvarshaanvar576@gmail.com>
2026-05-26 16:09:37 +0530

    Time: 1 ms (92.06%), Space: 56.6 MB (41.27%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.js
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.js
@@ -2,19 +2,30 @@
  * @param {string} word
  * @return {number}
  */
-var numberOfSpecialChars = function(word) {
-    let charCode = new Set()
-    let str = new Set(word.split("").join(""))
-    console.log(str)
+var numberOfSpecialChars = function (word) {
+    // let charCode = new Set()
+    // let str = new Set(word.split("").join(""))
+    // let count = 0
+    // for(let val of str){
+    //     let code = val.charCodeAt(0)
+    //     charCode.add(code)
+    //     let seen  =  code>96 ? code-32 : code+32
+    //     if(charCode.has(seen)){
+    //         count++
+    //     }
+    // }
+    // return count
+    let table = {}
     let count = 0
-    for(let val of str){
+    for (let val of word) {
         let code = val.charCodeAt(0)
-        charCode.add(code)
-        let seen  =  code>96 ? code-32 : code+32
-        if(charCode.has(seen)){
-            count++
+        if (!table[code]) {
+            table[code] = val
+            let seen = code > 96 ? code - 32 : code + 32
+            if (table[seen]) {
+                count++
+            }
         }
-    }
-    
-    return count
+
+    } return count
 };
