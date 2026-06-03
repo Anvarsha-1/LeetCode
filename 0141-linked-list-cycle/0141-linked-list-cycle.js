31f1d4444a53e3ed3851b0d3648a5a6cb8f317e4
Anvarsha <anvarshaanvar576@gmail.com>
2026-06-04 00:42:12 +0530

    Time: 34 ms (99.75%), Space: 59.5 MB (37.59%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -13,7 +13,7 @@
 var hasCycle = function(head) {
     let slow = head
     let fast = head
-    while(fast && fast.next){
+    while(slow && fast && fast.next){
         fast = fast.next.next
         slow = slow.next
         if(fast===slow){
