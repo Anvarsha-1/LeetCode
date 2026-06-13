faae2f532724ab17e797ebd05a5d77321bfd89a5
Anvarsha <anvarshaanvar576@gmail.com>
2026-06-13 10:23:59 +0530

    Time: 3 ms (56.74%), Space: 67.3 MB (97.78%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -5,17 +5,29 @@
  */
 var findMaxAverage = function(nums, k) {
     
-    let length = nums.length
-    let maxAverage = -Infinity
-    for(let i=0;i<=length-k;i++){
-        let sum = 0
-        let average = 0
-        for(let j=i;j<k+i;j++){
-           sum+=nums[j]
-        }
-        average =  sum/k
-        if(average>maxAverage){
-            maxAverage =  average
-        }
-    }return maxAverage
+    // let length = nums.length
+    // let maxAverage = -Infinity
+    // for(let i=0;i<=length-k;i++){
+    //     let sum = 0
+    //     let average = 0
+    //     for(let j=i;j<k+i;j++){
+    //        sum+=nums[j]
+    //     }
+    //     average =  sum/k
+    //     if(average>maxAverage){
+    //         maxAverage =  average
+    //     }
+    // }return maxAverage
+    let sum = 0
+    let maxSum = 0
+    for(let i=0;i<k;i++){
+        sum+=nums[i]
+    }
+    maxSum = sum
+
+    for(let j=k;j<nums.length;j++){
+        sum =  sum-nums[j-k] + nums[j]
+        maxSum =  Math.max(sum,maxSum)
+    }
+    return maxSum/k
 };
