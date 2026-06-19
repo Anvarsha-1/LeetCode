c73996edb207a87e5c35df60d526ccc1e27cab8e
Anvarsha <anvarshaanvar576@gmail.com>
2026-06-19 18:11:07 +0530

    Time: 0 ms (100%), Space: 54 MB (28.33%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} gain
+ * @return {number}
+ */
+var largestAltitude = function(gain) {
+    let altitudes =[0];
+    for(i=0;i<gain.length;i++){
+        altitudes.push(gain[i]+altitudes[i])
+    }return Math.max(...altitudes)
+};
