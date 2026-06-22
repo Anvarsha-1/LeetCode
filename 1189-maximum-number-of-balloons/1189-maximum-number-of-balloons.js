d456c1375fefc180225b9cf4e0213a137e02212f
Anvarsha <anvarshaanvar576@gmail.com>
2026-06-22 13:09:24 +0530

    Time: 12 ms (6.92%), Space: 55.5 MB (79.23%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} text
+ * @return {number}
+ */
+var maxNumberOfBalloons = function(text) {
+    let target = "baloon"
+    let mapTarget = { n: 1, l: 2, a: 1, b: 1, o: 2 }
+    let freq = {}
+    for(let i=0;i<text.length;i++){
+        if(target.includes(text[i])){
+           freq[text[i]] = (freq[text[i]] || 0)+1
+        }
+    }
+    let result = Infinity
+    console.log(freq)
+    if(Object.keys(freq).length<Object.keys(mapTarget).length) return 0
+    for(let char in freq){
+        min =  Math.floor(freq[char]/mapTarget[char])
+        if(min===0)return 0
+        if(result>min){
+            result = min
+        }
+    }
+   return result
+};
