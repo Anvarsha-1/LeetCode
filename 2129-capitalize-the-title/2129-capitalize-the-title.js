287b50caa3226ab7940fe34f42c1027112b46df8
Anvarsha <anvarshaanvar576@gmail.com>
2026-06-23 10:48:23 +0530

    Time: 67 ms (2.31%), Space: 63.6 MB (5.56%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.js
+++ 2129-capitalize-the-title/2129-capitalize-the-title.js
@@ -3,23 +3,41 @@
  * @return {string}
  */
 var capitalizeTitle = function(title) {
-    let ArrString = title.split(" ")
-    let str = ""
-    console.log(ArrString)
-    for(let i=0;i<ArrString.length;i++){
-        let len = ArrString[i].length
-        if(len>2){
-          for(let j=0;j<len;j++){
-            if(j==0){
-              str += ArrString[i][j].toUpperCase()
-            }else{
-                str+=ArrString[i][j].toLowerCase()
-            }
-          }
-          str+=" "
-        }else{
-           str+= ArrString[i].toLowerCase() + " "
-        }  
+//     let ArrString = title.split(" ")
+//     let str = ""
+    
+//     for(let i=0;i<ArrString.length;i++){
+//         let len = ArrString[i].length
+//         if(len>2){
+//           for(let j=0;j<len;j++){
+//             if(j==0){
+//               str += ArrString[i][j].toUpperCase()
+//             }else{
+//                 str+=ArrString[i][j].toLowerCase()
+//             }
+//           }
+//           str+=" "
+//         }else{
+//            str+= ArrString[i].toLowerCase() + " "
+//         }  
+//     }
+//    return str.trim()
+
+let capitalized = false
+let str = ""
+for(let i=0;i<title.length;i++){
+    if(title[i-1]==" "){
+        str+=""
+      capitalized = false
     }
-   return str.trim()
+    console.log(capitalized )
+    
+    if(!capitalized && title[i+1]!==" " && title[i+2]!==" " && i+2!==title.length & i+1!==title.length){
+           str +=  title[i].toUpperCase()
+           capitalized = true
+    }else {
+        str+=title[i].toLowerCase()
+    }
+}
+    return str
 };
