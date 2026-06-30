7ca1f6cc544578a6e3153ef5ce1188f727bd4f32
Anvarsha <anvarshaanvar576@gmail.com>
2026-06-30 17:58:01 +0530

    Time: 49 ms (74.8%), Space: 56.6 MB (62.42%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.js
+++ 0133-clone-graph/0133-clone-graph.js
@@ -20,11 +20,15 @@ var cloneGraph = function (node) {
         if (visited.has(node.val)) {
             return visited.get(node.val)
         }
-        let  root = new _Node(node.val)
+         let  root = new _Node(node.val)
         visited.set(node.val, root)
 
         for (let nei of node.neighbors) {
+            if (!visited.has(nei.val)) {
                root.neighbors.push(dfs(nei, visited))
+            }else{
+                root.neighbors.push(visited.get(nei.val))
+            }
         } return root
     }
     
