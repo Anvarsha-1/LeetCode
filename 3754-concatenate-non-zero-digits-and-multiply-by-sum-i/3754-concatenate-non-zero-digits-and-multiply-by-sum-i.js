4d36661d3bb9e7ce732d0eceb02ef53d68d7b5f4
Anvarsha <anvarshaanvar576@gmail.com>
2026-07-07 19:11:55 +0530

    Time: 1 ms (89.29%), Space: 55 MB (89.29%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.js
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.js
@@ -3,16 +3,27 @@
  * @return {number}
  */
 var sumAndMultiply = function(n) {
+//   let str = n+""
+//   let digit = ""
+//   let sum = 0
+//   for(let val of str){
+//     if(val!=0){
+//       digit+=val
+//       sum+=Number(val)
+//     }
+//   }
+//   return Number(digit*sum)
 
-  let str = n+""
-  let digit = ""
-  let sum = 0
-  for(let val of str){
-    if(val!=0){
-      digit+=val
-      sum+=Number(val)
+  let x = 0;
+  let sum = 0;
+  let i = 1
+  while(n){
+    if(n%10!==0){
+        sum+=n%10
+        x += (n%10)*i
+        i*=10
     }
-  }
-  return Number(digit*sum)
+    n = Math.floor(n/10)
+  }return x*sum
   
 };
